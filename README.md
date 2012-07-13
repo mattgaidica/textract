2c03e1632ed339cfe9dbe72ae42246b32a62ca57
Matt <matt@primestudiosllc.com>
2012-07-12 19:40:57 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+A suite of text extraction tools.
+
+*Make sure to include necessary gems in projects Gemfile
---
+++ extractor.rb
@@ -0,0 +1,145 @@
+require 'stanford-core-nlp'
+require 'amatch'
+include Amatch
+
+class Extractor
+  attr_accessor :text
+  def initialize text
+    @text = text.force_encoding('UTF-8')
+  end
+
+  def all
+    # since telephone regex is more strict, remove duplicates
+    books = self.books
+    # no need, but for the future string.scan(/\d+/).join to remove all but numbers
+    telephones = self.telephones
+    books.delete_if {|x| telephones.include?(x)}
+
+    {
+      :books => books,
+      :telephones => telephones,
+      :emails => self.emails
+    }
+  end
+
+  def people_to_emails
+    emails = self.emails
+    pipeline =  StanfordCoreNLP.load(:tokenize, :ssplit, :pos, :lemma, :parse, :ner, :dcoref)
+    text = StanfordCoreNLP::Text.new(@text)
+    pipeline.annotate(text)
+
+    entities = []
+    text.get(:sentences).each do |sentence|
+      # Syntatical dependencies
+      #puts sentence.get(:basic_dependencies).to_s
+      sentence.get(:tokens).each do |token|
+        entity = token.get(:named_entity_tag).to_s
+        if entity.downcase.eql?('person')
+          entities << {
+            :entity => entity.downcase,
+            :value => token.get(:value).to_s,
+            :start => token.get(:character_offset_begin).to_s.to_i,
+            :end => token.get(:character_offset_end).to_s.to_i
+          }
+        end
+      end
+    end
+
+    last_pos = nil
+    buffer = ''
+    temp = []
+    results = {}
+
+    entities.each_with_index do |entry, i|
+      if last_pos.nil? #first round
+        buffer << entry[:value]
+      else
+        # are the entries 1 character away? is that character a space?
+        puts "start minus last pos: #{(entry[:start] - last_pos)}"
+        puts "raw text: #{@text.slice(last_pos)}"
+        puts "last_pos: #{last_pos}"
+        if (entry[:start] - last_pos) == 1 && @text.slice(last_pos).eql?(' ')
+          buffer << " #{entry[:value]}"
+        else
+          #is there a word after it?
+          #is the next sequence a space and a capitalized letter?
+          # -> add it to buffer as name
+          temp << buffer
+          buffer = entry[:value]
+        end
+      end
+      last_pos = entry[:end]
+    end
+    temp << buffer
+    results[:person] = temp.uniq
+
+    email_rankings = []
+    temp = {}
+    emails.each do |email|
+      results[:person].each do |person|
+        m = Levenshtein.new(email.split('@').first.downcase.scan(/[a-z]/).join(''))
+        #could try averaging matches with just their initials, last name, etc.
+        tests = []
+        tests << m.match(person.downcase.scan(/[a-z]/).join(''))
+        tests << m.match("#{person.split(' ').first.downcase[0]}#{person.split(' ').last.downcase[0]}")
+        tests << m.match(person.split(' ').first.downcase)
+        tests << m.match(person.split(' ').last.downcase)
+        temp[person] = tests.min
+      end
+      email_rankings << temp.sort_by {|k,v| v}
+      temp = {}
+    end
+
+    assignments = {}
+    while(assignments.keys.count < [emails.count, results[:person].count].min)
+      email_rankings.each_with_index do |email_ranking, i|
+        next if assignments.has_value?(emails[i]) #next if email is assigned
+        email_ranking.each do |person_ranking|
+          next if assignments.has_key?(person_ranking[0]) #next if name is assigned
+          found_lower = false
+          email_rankings.each_with_index do |email_ranking_temp, j|
+            next if assignments.has_value?(emails[j]) #next if email is assigned
+            email_ranking_temp.each do |person_ranking_temp|
+              if person_ranking_temp[0] == person_ranking[0] && person_ranking_temp[1] < person_ranking[1]
+                found_lower = true
+              end
+            end
+          end
+          if found_lower == false
+            assignments[person_ranking[0]] = emails[i]
+            break
+          end
+        end
+      end
+    end
+    
+    assignments
+  end
+
+  def books
+    isbns = []
+    self.text.scan(/(?<isbn>(\d[- ]?){9,12}([0-9xX]))/).each {|x| isbns << x[0]}
+    isbns
+  end
+
+  def telephones
+    telephones = []
+    # comes out [prefix, XXX, XXX, XXXX, extension]
+    self.text.scan(/\s(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?\s/).each do |x|
+      phone = ""
+      phone << "#{x[0]}-" unless x[0].nil?
+      phone << "#{x[1]}-" unless x[1].nil?
+      phone << "#{x[2]}-#{x[3]}"
+      phone.gsub!(' ', '')
+      phone << " x#{x[4]}" unless x[4].nil?
+      telephones << phone
+    end
+    telephones
+  end
+
+  def emails
+    emails = []
+    self.text.scan(/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i).each {|x| emails << x}
+    emails
+  end
+end
